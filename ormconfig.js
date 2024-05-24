7da05e640f313605b456e8f0e5679d3153b76547
dleon-prog <156679389+dleon-prog@users.noreply.github.com>
2024-05-24 09:22:16 -0300

    Add files via upload

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'default',
   type: 'mysql',
-  url: 'mysql2://root:@localhost:3306/factory',
-  database: 'factory',
+  url: 'mysql2://root:@localhost:3306/postgre',
+  database: 'postgre',
   migrations: ['./src/migrations/*.ts'],
   cli: {
     migrationsDir: './src/migrations/'
